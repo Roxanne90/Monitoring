fc060204dd2439706a6d79a4ccd82b270b3f81da
Roxanne CMT <62661691+Roxanne90@users.noreply.github.com>
2020-05-13 17:55:19 +0200

    Update R_code_faPAR.r

--- R_code_faPAR.r
+++ R_code_faPAR.r
@@ -24,11 +24,27 @@ pdf("copNDVI.pdf")
 levelplot(copNDVI)
 dev.off()
 
- 
-
 pdf("faPAR.pdf")
 levelplot(faPAR10)
 dev.off()
 
- 
+################ day 2
+
+library(ratser)
+library(rastediv)
+library(rasterVis)
+
+setwd("Users/utente/lab")
+load("faPAR.RData")
+
+# the original faPAR from Copernicus is 2GB
+# Let's see how much space is needed for 8-bit set
+
+writeRaster(copNDVI, "copNDVI.tif")
+
+#5.5mb
+#faPAR: levelplot this set
+
+faPAR10 <- raster("copNDVI.tif")
+levelplot(faPAR10)
 
